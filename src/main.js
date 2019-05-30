80ba9e385946d4bcdb3cbefd25f9dd72d4c7af14
Abubakar Sambo <sadiqasg@gmail.com>
2019-05-30 23:34:57 +0100

    implements get employee by id

--- src/main.js
+++ src/main.js
@@ -15,16 +15,18 @@ $(function() {
         $("#numberOfemployees").html(total);
 
         for (var i in result) {
-          records += `<tr>
-                      <td>${result[i].id}</td>
-                      <td>${result[i].firstName}</td>
-                      <td>${result[i].lastName}</td>
-                      <td>${result[i].email}</td>
-                      <td>${result[i].gender}</td>
+          records += `<tr id='${result[i].id}'>
+                      <td class='table-rows'>${result[i].id}</td>
+                      <td class='table-rows'>${result[i].firstName}</td>
+                      <td class='table-rows'>${result[i].lastName}</td>
+                      <td class='table-rows'>${result[i].email}</td>
+                      <td class='table-rows'>${result[i].gender}</td>
                       <td><button class='btn btn-danger'>delete</button></td></tr>
                   `;
 
-          employees += `<a class='btn btn-outline-success m-1' id='${result[i].firstName} ${result[i].lastName}' onclick='attndBtn(this.id)'>${
+          employees += `<a class='btn btn-outline-success m-1' id='${
+            result[i].firstName
+          } ${result[i].lastName}' onclick='attndBtn(this.id)'>${
             result[i].firstName
           } ${result[i].lastName}</a>`;
         }
@@ -52,7 +54,7 @@ $(function() {
           history += `<tr>
                       <td>${result[i].id}</td>
                       <td>${result[i].title}</td>
-                      <td><button class='btn btn-info' id='viewAttnd'>view</button></td></tr>
+                      <td><button class='btn btn-info viewId'>view</button></td></tr>
                  `;
         }
 
@@ -61,7 +63,26 @@ $(function() {
       error: err => console.log("error", err)
     });
   };
-  
+
+  $(document).on("click", ".table-rows", function() {
+    let id = $(this)
+      .parent()
+      .attr("id");
+
+    $.ajax({
+      type: "GET",
+      url: `${url}/employees/${id}`,
+      success: data => {
+        $("#employeeNumber").text(`${data.id}`);
+        $("#employeeGender").text(`${data.gender}`);
+        $("#employeeName").text(`${data.firstName} ${data.lastName}`);
+        $("#employeeMail").text(`${data.email}`);
+        $(".myBtn").click();
+      },
+      error: e => console.log(e)
+    });
+  });
+
   // create/save employee
   $("#saveEmployee").on("click", e => {
     e.preventDefault();
@@ -92,41 +113,58 @@ $(function() {
   });
 
   // attendance record
-  $('#saveEmployeeAttendance').on('click', (e) => {
+  $("#saveEmployeeAttendance").on("click", e => {
     e.preventDefault();
-    let title = $('#title').val();
+    let title = $("#meetingTitle").val();
     if (!title) return;
 
-    let attnd = ['sadiq', 'femi', 'john', 'doe']
     let meeting = {
       title: title,
-      attendees: attnd
-    }
+      attendees: attendees
+    };
 
     $.ajax({
-      type: 'POST',
+      type: "POST",
       url: `${url}/meetings`,
       data: JSON.stringify(meeting),
-      dataType: 'json',
-      contentType: 'application/json',
-      success: (res) => {
-        console.log('success', res)
+      dataType: "json",
+      contentType: "application/json",
+      success: res => {
+        console.log("success", res);
+        $("#closeMeetingModal").click();
+        getEmployees();
+        $("#meetingTitle").val("");
       },
-      error: (err) => {
-        console.log('error', err)
+      error: err => {
+        console.log("error", err);
       }
     });
   });
 
   getEmployees();
   getHistory();
-  
-  
 }); //end jq
 
 // mark attendance
 let attendees = [];
 function attndBtn(id) {
-  document.getElementById(id).style.cssText = 'background-color:lightgreen;color: white;';
-  
+  document.getElementById(id).style.cssText =
+    "background-color:lightgreen;color: white;";
+  attendees.push(id);
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var btn = document.querySelector(".myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+span.onclick = function() {
+  modal.style.display = "none";
+};
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 };
